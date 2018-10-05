9cee14249b5bfe46dd7468a9617d0945d56f44d2
Boyan Mihaylov <hey@boyan.io>
2018-10-05 06:47:43 +0200

    Instantiate the wasm with a specific module rather than the default global Module

--- build.sh
+++ build.sh
@@ -29,10 +29,11 @@ emmake make
 mkdir out
 emcc -O3 -I . -I Zend -I main -I TSRM/ ../pib_eval.c -o pib_eval.o
 emcc -O3 \
-  -s WASM=1 \
   -s ENVIRONMENT=web \
   -s EXPORTED_FUNCTIONS='["_pib_eval", "_php_embed_init", "_zend_eval_string", "_php_embed_shutdown"]' \
   -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall"]' \
+  -s MODULARIZE=1 \
+  -s EXPORT_NAME="'PHP'" \
   -s TOTAL_MEMORY=134217728 \
   -s ASSERTIONS=0 \
   -s INVOKE_RUN=0 \
