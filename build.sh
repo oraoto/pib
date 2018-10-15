f8517362c3137f7f04c7ffedee8c7ac097c66d51
Oraoto <oraoto@outlook.com>
2018-10-15 10:08:42 +0800

    set ERROR_ON_UNDEFINED_SYMBOLS=0

--- build.sh
+++ build.sh
@@ -45,6 +45,7 @@ emcc -O3 \
   -s TOTAL_MEMORY=134217728 \
   -s ASSERTIONS=0 \
   -s INVOKE_RUN=0 \
+  -s ERROR_ON_UNDEFINED_SYMBOLS=0 \
   --preload-file Zend/bench.php \
   libs/libphp7.a pib_eval.o -o out/php.html
 
