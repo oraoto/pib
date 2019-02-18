da9a0a2e8fb0ab5efdcdf9181bd313337bd14503
Zack Rothauser <zrothauser@gmail.com>
2019-02-18 11:50:54 -0600

    Add minimal theme to repo

---
+++ minimal-theme/index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for showing posts.
+ *
+ * @package minimal-theme
+ */
+
+?><!DOCTYPE html>
+<html>
+<head>
+</head>
+
+<body>
+    <?php
+    echo 'Test that the theme is rendering.';
+    // Uncomment to enable the loop:
+	// if ( have_posts() ) {
+	// 	while ( have_posts() ) {
+	// 		the_post();
+	// 		the_content();
+	// 	}
+	// }
+	?>
+</body>
+</html>
